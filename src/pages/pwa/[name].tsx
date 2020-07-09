df9b34d7654f61444e34967d54039117879a6488
nrgapple <champtoday@gmail.com>
2020-07-09 11:55:02 -0400

    use server side fetching

--- src/pages/pwa/[name].tsx
+++ src/pages/pwa/[name].tsx
@@ -2,38 +2,46 @@ import { useRouter } from "next/dist/client/router"
 import { useEffect, useState } from "react"
 import Layout from "../../components/Layout"
 import Head from "next/head"
+import { InferGetServerSidePropsType, GetServerSideProps } from "next"
+import context from "react-bootstrap/esm/AccordionContext"
 
-const PWA = () => {
-  const router = useRouter()
-  const { name } = router.query
-
-  const [pwa, setPwa] = useState<any>()
-
-  useEffect(() => {
-    if (name) {
-      const getData = async () => {
-        const resp = await fetch(
-          `https://progressiveapp.store/api/public/pwa/${name}`
-        )
-        const app = await resp.json()
-        setPwa(app)
-      }
-      getData()
-    }
-  }, [name])
-
+// For info on server side props:
+// https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+const PWA = ({
+  app,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(app)
   return (
     <Layout>
       <Head>
-        {pwa && (
+        {app && (
           <>
-            <title>{pwa.name}</title>
+            <title>{app.name}</title>
             <link></link>
           </>
         )}
       </Head>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  if (context.params && context.params.name) {
+    const resp = await fetch(
+      `https://progressiveapp.store/api/public/pwa/${context.params.name}`
+    )
+    const app = await resp.json()
+    console.log(app)
+    return {
+      props: {
+        app,
+      },
+    }
+  } else {
+    return {
+      props: {},
+    }
+  }
 }
 
 // not sure if this is even possible
