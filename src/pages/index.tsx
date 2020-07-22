caf14a70c82c49f10d748dece9c57971eaae8004
Matt <mjruddy94@gmail.com>
2020-07-21 20:37:16 -0400

    add pest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,99 +12,63 @@ const Home = () => {
   return (
     <Layout>
       <Head>
-        <title>Progressive App Store</title>
+        <title>NoSpray</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar className="navbar navbar-expand-md navbar-default" sticky="top">
         <Container>
           <NavbarBrand className="HomeNavBrand">
-            <img src="/store.png" height="50px" className="logo-header" />
             <h1 className="pl-3">{Branding.storeName}</h1>
           </NavbarBrand>
         </Container>
       </Navbar>
-      <section>
-        <div className="container-fluid">
-          <Row className="header pb-5">
-            <Col
-              xs={12}
-              className="align-self-center ml-auto mb-5 text-center pl-3 pr-3"
-            >
-              {" "}
-              <h1 className="fw-600">Progressive App Store</h1>
-              <p className="lead-2">
-                World's largest store of developer made web apps.
-              </p>
-              <Button href={`${storeUris.base}${storeUris.login}`}>
-                Sign Up
-              </Button>
-            </Col>
-            <Col xs={12} className="ml-auto mt-5 video-landing-full">
-              <video autoPlay loop playsInline>
-                <source src="/videos/devlog.mp4" />
-              </video>
-            </Col>
-          </Row>
-        </div>
-      </section>
-      <main>
-        <section className="section bg-alternate overflow-hidden">
-          <div className="container-fluid pb-5">
-            <header className="section-header mt-5">
-              <h1 className="lead-5 fw-600">Follow Your Favorate Web Apps</h1>
-              <p className="lead-4">
-                Developers can post DevLogs to update you on behind the scenes
-                developerment.
-              </p>
-            </header>
-            <Row className="align-items-center">
-              <Col xs={12} md={4} className="pl-5">
-                <h5 className="text-center text-md-left lead-5 fw-600">
-                  Add DevLog
-                </h5>
-                <p className="lead-2 text-center text-md-left">
-                  Just enter your app and a quick markdown body.
-                </p>
-              </Col>
-              <Col xs={12} md={8} className="text-center video-landing-small">
-                <video autoPlay loop playsInline>
-                  <source src="/videos/devlog.mp4" />
-                </video>
-              </Col>
-            </Row>
-            <div className="space-block" />
-            <Row className="align-items-center">
-              <Col xs={12} md={8} className="text-center video-landing-small">
-                <video autoPlay loop playsInline>
-                  <source src="/videos/devlog.mp4" />
-                </video>
-              </Col>
-              <Col xs={12} md={4} className="pl-5">
-                <h5 className="text-center text-md-left lead-5 fw-600">
-                  Add DevLog
-                </h5>
-                <p className="lead-2 text-center text-md-left">
-                  Just enter your app and a quick markdown body.
-                </p>
-              </Col>
-            </Row>
-          </div>
-        </section>
-        <section className="section pt-5">
-          <Row className="row-gap-y text-center text-default justify-content-md-center mt-5">
-            <Col xs={12}>
-              <h5 className="lead-3">Login or Sign up now!</h5>
-            </Col>
-            <Col xs={12}>
-              <ButtonGroup>
-                <Button>Login</Button>
-                <Button>Sign Up</Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-        </section>
-        <div className="space-block" />
-      </main>
+      <div className="container-fluid">
+        <Row className="header pb-5 justify-content-md-center">
+          <Col
+            xs={12}
+            className="align-self-center ml-auto mb-5 text-center pl-3 pr-3"
+          >
+            {" "}
+            <h1 className="fw-600">NoSpray</h1>
+            <p className="lead-2">Hypersentitivity Individual Registrar</p>
+            <Button href={`https://nospray.us/signup`}>Sign Up</Button>
+          </Col>
+          <Col xs={8}>
+            <img height="100%" width="100%" src="store.png" />
+          </Col>
+        </Row>
+      </div>
+      <Row className="row-gap-y text-center text-default justify-content-md-center mt-5">
+        <Col>
+          <h3>Pricing</h3>
+        </Col>
+      </Row>
+      <Row className="row-gap-y text-center text-default justify-content-md-center mt-5">
+        <Col>
+          <Card>
+            <Card.Header>Individual</Card.Header>
+            <Card.Body></Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Header>Company</Card.Header>
+            <Card.Body></Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="row-gap-y text-center text-default justify-content-md-center mt-5">
+        <Col xs={12}>
+          <h5 className="lead-3">Login or Sign up now!</h5>
+        </Col>
+        <Col xs={12}>
+          <ButtonGroup>
+            <Button>Login</Button>
+            <Button>Sign Up</Button>
+          </ButtonGroup>
+        </Col>
+      </Row>
+      <div className="space-block" />
       <footer className="footer">
         <Container>
           <Row>
--- src/util/banding.ts
+++ src/util/banding.ts
@@ -1,5 +1,5 @@
 const Branding = {
-  storeName: "ProgressApp.Store",
+  storeName: "NoSpray",
 }
 
 export default Branding
